113812a9b1a31f16839879440a01139cc5c8c190
Darmanbek <pahratdinovdarman@gmail.com>
2025-01-22 15:31:02 +0500

    Update Root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,21 @@
 import { QueryClient } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router"
-import { TanStackRouterDevtools } from "@tanstack/router-devtools"
+import React from "react"
+// import { TanStackRouterDevtools } from "@tanstack/router-devtools"
 import { AuthContext } from "src/context/auth-context"
+
+const TanStackRouterDevtools =
+	process.env.NODE_ENV === "production"
+		? () => null // Render nothing in production
+		: React.lazy(() =>
+				// Lazy load in development
+				import("@tanstack/router-devtools").then((res) => ({
+					default: res.TanStackRouterDevtools
+					// For Embedded Mode
+					// default: res.TanStackRouterDevtoolsPanel
+				}))
+			)
 
 export const Route = createRootRouteWithContext<{
 	queryClient: QueryClient
